2aa700136bf12dc7638f98d9360e09f47e64ac3b
Austin Adamson <austin.l.adamson@gmail.com>
2019-01-08 22:07:17 -0700

    Version 0.0.6

---
+++ src/CPP/EchoHandler.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace CPP;
+
+class EchoHandler {
+
+	public function scream()
+	{
+		echo 'AHHHHHHHHHH!!!!!!!';
+	}
+
+	public function sayHello()
+	{
+		echo 'Hello Kind Sir.';
+	}
+}
